b7e8bb3c09a59462598112ea1bde40392cdfaad8
administrator <administrator@administrators-MacBook-Air.local>
2011-02-22 18:32:51 -0600

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vid2::Application.config.secret_token = 'a9493eb79125ef59f42df7019dce41387d69aabb9ed5a3347d1c7c3e13e82227bb0d03aa4f841c44f4146f8ba0b4a3f2d8437b7abea250fd4170d1bee0368d87'
